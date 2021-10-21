445cf0dd16a35277724934a144dd0686d09bfd71
Josef Reichelt <pepareichelt@gmail.com>
2021-10-21 23:39:52 +0200

    Add basic node structure

---
+++ node/src/main.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const port = 1338;
+const App = express();
+
+App.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+App.post('/', (req, res) => {
+    res.send('Hello World!');
+});
+App.put('/', (req, res) => {
+    res.send('Hello World!');
+});
+App.delete('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+App.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
